5893d08cde8c10c5d03a95d3345da1ce2a9bd873
lanliwz <lanliwz@yahoo.com>
2025-04-28 20:02:13 -0400

    add family tree

--- gojs_models/ft/gojs_ft_init.py
+++ gojs_models/ft/gojs_ft_init.py
@@ -1,4 +1,3 @@
-from gojs_models.ft.gogs_ft_binding import BindingComponents
 from gojs_models.ft.gogs_ft_person_shape import PersonShapeComponents
 from gojs_models.ft.gojs_ft_template import TemplateComponents
 from gojs_models.ft.gojs_ft_theme_manager import *
@@ -6,12 +5,10 @@ from gojs_models.ft.gojs_ft_theme_manager import *
 def init():
 
     handlers = PartEventHandlers()
-    # bindings = BindingComponents()
     templates = TemplateComponents()
     person_shapes = PersonShapeComponents()
     return (
         handlers.to_javascript() + '\n'
-        # + bindings.to_javascript() + '\n'
         + person_shapes.to_javascript() + '\n'
         + templates.to_javascript() + '\n'
     )
